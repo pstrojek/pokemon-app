162e90af54e6a5a83b0a70529757236745bc0f73
Pawel Strojek <paulstrojek@gmail.com>
2019-12-15 17:32:08 +0100

    Add simple pokemon details logic

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { PokemonService } from '../services/pokemon.service';
+import { Pokemon } from '../types/pokemon';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-details.component.scss']
 })
 export class PokemonDetailsComponent implements OnInit {
+  pokemon$: Observable<Pokemon>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private pokemonService: PokemonService
+  ) {}
 
   ngOnInit() {
+    this.pokemon$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.pokemonService.getPokemon(Number(params.get('id'))))
+    );
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
   }
 
 }
