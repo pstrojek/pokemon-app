5d0c22940d38cfb0100ff157def489f09fcf29a6
Pawel Strojek <paulstrojek@gmail.com>
2019-12-19 18:19:05 +0100

    Add next and previous Pokemon buttons and logic

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { PokemonService } from '../services/pokemon.service';
 import { Pokemon } from '../types/pokemon';
@@ -13,16 +13,32 @@ import { Pokemon } from '../types/pokemon';
 })
 export class PokemonDetailsComponent implements OnInit {
   pokemon$: Observable<Pokemon>;
+  currentPokemonId: number;
 
   constructor(
     private route: ActivatedRoute,
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.pokemon$ = this.route.paramMap.pipe(
+      tap((params: ParamMap) => this.currentPokemonId = Number(params.get('id'))),
       switchMap((params: ParamMap) =>
         this.pokemonService.getPokemon(Number(params.get('id'))))
     );
+  }
+
+  nextPokemon() {
+    const nextPokemonId = this.currentPokemonId + 1;
+    this.router.navigate([`/pokemon/${nextPokemonId}`]);
+  }
+
+  previousPokemon() {
+    let previousPokemonId = this.currentPokemonId - 1;
+    if (previousPokemonId < 1) {
+      previousPokemonId = 1;
+    }
+    this.router.navigate([`/pokemon/${previousPokemonId}`]);
   }
 }
